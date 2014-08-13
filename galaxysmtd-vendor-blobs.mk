bfea71b580ee5e00315c3b7b6d3925150e301b65
martincz <martincz@mokeedev.com>
2014-08-13 22:38:58 +0800

    update vendor files
    Change-Id: If1cba1d267056882531130b11ad2545c5fe39434

--- galaxysmtd-vendor-blobs.mk
+++ galaxysmtd-vendor-blobs.mk
@@ -26,4 +26,5 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/galaxysmtd/proprietary/vendor/lib/libsensor_yamaha_test.so:system/vendor/lib/libsensor_yamaha_test.so \
     vendor/samsung/galaxysmtd/proprietary/vendor/bin/orientationd:system/vendor/bin/orientationd \
     vendor/samsung/galaxysmtd/proprietary/vendor/bin/geomagneticd:system/vendor/bin/geomagneticd \
+    vendor/samsung/galaxysmtd/proprietary/vendor/bin/Certification.cer:system/vendor/bin/Certification.cer \
     vendor/samsung/galaxysmtd/proprietary/modem.bin:modem.bin
